6381312c720903d627043e62233cbba9cc36e6d5
Tiffany <chirashii>
2019-04-26 00:33:59 -0700

    initial commit
    add all my shitty code from 10 years ago

---
+++ dragdrop.js
@@ -0,0 +1,20 @@
+/**
+  * Drag and drop functionality for checkers
+  */
+  
+var GhostChecker = function(color) {
+	var img;
+	if ( color == 'red' ) {
+		img = $('img').attr('src', 'graphics/red-piece.png');
+		console.log('red');
+	}
+	if ( color == 'black' ) {
+	
+	}
+	
+	
+	
+	
+	
+	return img;
+};
